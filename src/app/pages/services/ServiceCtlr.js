a4c5cacedad91910580cd96d372af8b10ba743d6
Your Name <you@example.com>
2018-08-09 08:03:54 +0100

    fix minor front end issues

--- src/app/pages/services/ServiceCtlr.js
+++ src/app/pages/services/ServiceCtlr.js
@@ -9,7 +9,6 @@
 
         $scope.serviceForm = {};
         $scope.editServiceForm = false;;
-        $scope.mytime = new Date();
 
         $scope.daysOfWeek = {
             "0": 'Sunday', "1": 'Monday', "2": "Tuesday", "3": "Wednesday", 4: "Thursday", "5": "Friday", "6": "Saturday"
